4dc2f17385cd70421c75617fcfd62fd95817a258
Muhammad-Tameem Mughal <thecodingpro100@gmail.com>
2022-07-24 19:27:19 -0400

    another

--- UBER/rdkit/Chem/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/bld/rdkit_1657061356439/work/rdkit/Chem
-# Build directory: D:/bld/rdkit_1657061356439/work/rdkit/Chem
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(pythonTestDirChem "C:/Users/theco/anaconda3/envs/deeplearning/python.exe" "D:/bld/rdkit_1657061356439/work/rdkit/Chem/test_list.py" "--testDir" "D:/bld/rdkit_1657061356439/work/rdkit/Chem" "--buildType" "Release")
-set_tests_properties(pythonTestDirChem PROPERTIES  _BACKTRACE_TRIPLES "D:/bld/rdkit_1657061356439/work/Code/cmake/Modules/RDKitUtils.cmake;212;add_test;D:/bld/rdkit_1657061356439/work/rdkit/Chem/CMakeLists.txt;1;add_pytest;D:/bld/rdkit_1657061356439/work/rdkit/Chem/CMakeLists.txt;0;")
